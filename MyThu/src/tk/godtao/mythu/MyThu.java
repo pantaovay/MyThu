de54b8adf0b4b9b87769eed3f92de97716b4f967
Pan Tao <pantaovay@gmail.com>
2013-05-22 00:40:47 +0800

    设置路径

--- MyThu/src/tk/godtao/mythu/MyThu.java
+++ MyThu/src/tk/godtao/mythu/MyThu.java
@@ -1,12 +1,23 @@
 package tk.godtao.mythu;
 
 //import java.io.Console;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -28,8 +39,45 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class MyThu {
+public class MyThu implements ActionListener {
+	static JFrame f = null;
+	JTextPane downloadInformation;
+	JTextField rootPath;
+	JButton begin;
+	String userid;
+	String userpass;
+
 	public MyThu(String userid, String userpass) throws Exception {
+		this.userid = userid;
+		this.userpass = userpass;
+		f = new JFrame("MyThu");
+		Container contentPane = f.getContentPane();
+
+		JPanel panel = new JPanel();
+
+		rootPath = new JTextField("设置根目录");
+		panel.add(rootPath);
+
+		downloadInformation = new JTextPane();
+		panel.add(downloadInformation);
+
+		begin = new JButton("开始");
+		panel.add(begin);
+		begin.addActionListener(this);
+
+		contentPane.add(panel);
+		f.getRootPane().setDefaultButton(begin);
+		f.pack();
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				System.exit(0);
+			}
+		});
+		f.setVisible(true);
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
 		// 输入用户名和密码
 		/*
 		 * Scanner sc = new Scanner(System.in); System.out.print("请输入用户名：");
@@ -38,32 +86,30 @@ public class MyThu {
 		 */
 
 		// 屏蔽密码输出，但是需要真正的console支持
-		//Console cons = System.console();
-		//String userid = cons.readLine("请输入用户名：");
-		//String userpass = new String(cons.readPassword("请输入密码："));
-
-		// 登陆
-		SchemeRegistry schemeRegistry = new SchemeRegistry();
-		schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory
-				.getSocketFactory()));
-		schemeRegistry.register(new Scheme("https", 80, PlainSocketFactory
-				.getSocketFactory()));
-		ClientConnectionManager cm = new PoolingClientConnectionManager(
-				schemeRegistry);
-		HttpClient httpclient = new DefaultHttpClient(cm);
-
-		HttpPost httpPost = new HttpPost(
-				"https://learn.tsinghua.edu.cn/MultiLanguage/lesson/teacher/loginteacher.jsp");
-		List<NameValuePair> login = new ArrayList<NameValuePair>();
-		login.add(new BasicNameValuePair("userid", userid));
-		login.add(new BasicNameValuePair("userpass", userpass));
-		login.add(new BasicNameValuePair("submit1", "登陆"));
-		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(login, "utf-8");
-		httpPost.setEntity(entity);
-
-		HttpResponse loginResponse = httpclient.execute(httpPost);
-
+		// Console cons = System.console();
+		// String userid = cons.readLine("请输入用户名：");
+		// String userpass = new String(cons.readPassword("请输入密码："));
 		try {
+			SchemeRegistry schemeRegistry = new SchemeRegistry();
+			schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory
+					.getSocketFactory()));
+			schemeRegistry.register(new Scheme("https", 80, PlainSocketFactory
+					.getSocketFactory()));
+			ClientConnectionManager cm = new PoolingClientConnectionManager(
+					schemeRegistry);
+			HttpClient httpclient = new DefaultHttpClient(cm);
+
+			HttpPost httpPost = new HttpPost(
+					"https://learn.tsinghua.edu.cn/MultiLanguage/lesson/teacher/loginteacher.jsp");
+			List<NameValuePair> login = new ArrayList<NameValuePair>();
+			login.add(new BasicNameValuePair("userid", userid));
+			login.add(new BasicNameValuePair("userpass", userpass));
+			login.add(new BasicNameValuePair("submit1", "登陆"));
+			UrlEncodedFormEntity entity = new UrlEncodedFormEntity(login,
+					"utf-8");
+			httpPost.setEntity(entity);
+
+			HttpResponse loginResponse = httpclient.execute(httpPost);
 			HttpEntity loginEntity = loginResponse.getEntity();
 			if (loginEntity != null) {
 				String loginResult = EntityUtils.toString(loginEntity);
@@ -84,7 +130,8 @@ public class MyThu {
 						Pattern pattern = Pattern.compile(regEx);
 						Element course = coursesIterator.next();
 						// 每个课程建立文件夹
-						String courseName = course.html();
+						String courseName = rootPath.getText() + "/";
+						courseName += course.html();
 						File courseDir = new File(courseName);
 						if (!courseDir.isDirectory()) {
 							courseDir.mkdir();
@@ -105,7 +152,8 @@ public class MyThu {
 									.select("a[href~=.*uploadFile.*]");
 							Iterator<Element> courseWaresIterator = courseWares
 									.iterator();
-							System.out.println("下载到文件夹  "+courseName+"......");
+							System.out.println("下载到文件夹  " + courseName
+									+ "......");
 							while (courseWaresIterator.hasNext()) {
 								Element courseWaresLink = courseWaresIterator
 										.next();
@@ -132,8 +180,8 @@ public class MyThu {
 				}
 			}
 			EntityUtils.consume(loginEntity);
-		} finally {
-			httpPost.releaseConnection();
+		} catch (Exception e1) {
+			System.out.println(e1.getMessage());
 		}
 	}
 }
